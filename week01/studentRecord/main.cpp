5eb49ccf500fa843aecc6354e5c479615675c365
Gyuhyun Lim <hyunxx14@gmail.com>
2024-01-28 13:57:30 +0900

    add studentRecord directory

---
+++ week01/studentRecord/main.cpp
@@ -0,0 +1,8 @@
+#include <fstream>
+#include <ios>
+#include <iostream>
+#include "studentRecord.hpp"
+
+int main(void)
+{
+}
---
+++ week01/studentRecord/studentRecord.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef STUDENT_RECORD_HPP
+#define STUDENT_RECORD_HPP
+
+struct StudentRecord
+{
+    int grade;
+    char name[20];
+};
+
+#endif /* STUDENT_RECORD_HPP */
